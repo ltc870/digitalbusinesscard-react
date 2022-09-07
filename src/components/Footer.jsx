8b2b9ab6553429a9bce4b6af82bdaf42626d6475
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-07 18:31:46 -0400

    added markup to Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <a href="https://www.twitter.com/heymrcaudle"><i class="fa-brands fa-square-twitter"></i></a>
+            <a href="https://www.facebook.com/lcaudle07"><i class="fa-brands fa-square-facebook"></i></a>
+            <a href="https://www.linkedin.com/in/lawrence-caudle-ba0492b7"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="https://www.github.com/ltc870"><i class="fa-brands fa-square-github"></i></a>
+        </footer>
+    )
+}
