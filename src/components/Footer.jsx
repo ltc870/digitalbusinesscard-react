1f0a975fc4804f9c05e3da786d668aa20bdc4e52
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-07 18:40:00 -0400

    added classes to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Footer() {
     return (
         <footer className="footer footer-flex">
-            <a href="https://www.twitter.com/heymrcaudle"><i class="fa-brands fa-square-twitter"></i></a>
-            <a href="https://www.facebook.com/lcaudle07"><i class="fa-brands fa-square-facebook"></i></a>
-            <a href="https://www.linkedin.com/in/lawrence-caudle-ba0492b7"><i class="fa-brands fa-linkedin"></i></a>
-            <a href="https://www.github.com/ltc870"><i class="fa-brands fa-square-github"></i></a>
+            <a className="footer-social-link footer-twitter" href="https://www.twitter.com/heymrcaudle"><i class="fa-brands fa-square-twitter"></i></a>
+            <a className="footer-social-link footer-facebook" href="https://www.facebook.com/lcaudle07"><i class="fa-brands fa-square-facebook"></i></a>
+            <a className="footer-social-link footer-linkedin" href="https://www.linkedin.com/in/lawrence-caudle-ba0492b7"><i class="fa-brands fa-linkedin"></i></a>
+            <a className="footer-social-link footer-github" href="https://www.github.com/ltc870"><i class="fa-brands fa-square-github"></i></a>
         </footer>
     )
 }
