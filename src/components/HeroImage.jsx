ec1b5a80b074e00e35d0f08f006a27e376447e31
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-07 17:56:02 -0400

    added jsx files to components folder

---
+++ src/components/HeroImage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HeroImage() {
+    return (
+        <header className="hero-image-container">
+            <img className="hero-image" src="./src/images/professional.jpeg" alt="" />
+        </header>
+    )
+}
