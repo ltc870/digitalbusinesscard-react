9544fbb6e0f9005ba495e810a025712cbd41e1d8
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-07 18:10:40 -0400

    added markup for Main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <main className="main">
+            <section className="main-section">
+                <h1 className="main-title">Lawrence Caudle</h1>
+                <p className="main-subtitle">Frontend Developer</p>
+                <a className="main-link" href="https://www.lawrencecaudle.com/" target="_blank">www.lawrencecaudle.com</a>
+                <button className="main-button"><a href = "mailto: abc@example.com"><i class="fa-solid fa-envelope"></i></a></button>
+            </section>
+            <section className="about-section">
+                <h2>About</h2>
+                <p>This is my about</p>
+            </section>
+            <section className="interest-section">
+                <h2>Interest</h2>
+                <p>This is my interest section</p>
+            </section>
+        </main>
+    )
+}
