a26f67b9ccf70e7df184afdc1deb301100fc4331
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-07 08:15:34 -0400

    deleted css import/it was in wrong file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
 
 function App() {
 
   return (
     <div className='container'>
-
+      <h1>Hello World!</h1>
     </div>
   ) 
 }
