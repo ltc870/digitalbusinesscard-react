ed415f70d9dee06f69e6cd30706545ee826961ca
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-07 18:33:33 -0400

    imported Footer.jsx into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import React from "react";
 import HeroImage from './components/HeroImage';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <h1>Hello World!</h1>
       <HeroImage />
       <Main />
+      <Footer />
     </div>
   ) 
 }
