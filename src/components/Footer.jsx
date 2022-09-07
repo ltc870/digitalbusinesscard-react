c418c09d642d4e74c9073f8961f653f89b01aefc
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-07 18:36:34 -0400

    added footer-flex class to footer in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="footer">
+        <footer className="footer footer-flex">
             <a href="https://www.twitter.com/heymrcaudle"><i class="fa-brands fa-square-twitter"></i></a>
             <a href="https://www.facebook.com/lcaudle07"><i class="fa-brands fa-square-facebook"></i></a>
             <a href="https://www.linkedin.com/in/lawrence-caudle-ba0492b7"><i class="fa-brands fa-linkedin"></i></a>
