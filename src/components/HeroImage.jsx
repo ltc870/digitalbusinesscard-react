d1487996b44c3c7c94528aafc4424f921214896b
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-08 09:42:02 -0400

    moved images folder from src to public

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HeroImage() {
     return (
         <header className="hero">
-            <img className="hero-image" src="./src/images/professional.jpeg" alt="" />
+            <img className="hero-image" src="./images/professional.jpeg" alt="" />
         </header>
     )
 }
